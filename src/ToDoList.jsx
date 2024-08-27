3ac24c8fd24f5627ddaad712c2718910b8dd2e9d
Rcparsln811 <receparslan811@gmail.com>
2024-08-27 16:41:56 +0300

    add

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './index.css';
 import './App.css';
+import { motion } from "framer-motion"
 
 function ToDoList() {
 
@@ -35,6 +36,7 @@ function ToDoList() {
         <div className="to-do-list" >
 
             <h1>To-Do-List</h1>
+
             <div>
                 <input
                     type="text"
