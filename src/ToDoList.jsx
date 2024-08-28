00b7dbb85030de31cc48e4b0d13bd65f25e9e967
Rcparsln811 <receparslan811@gmail.com>
2024-08-28 17:13:39 +0300

    some updates

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import './index.css';
 import './App.css';
-import { motion, Reorder } from "framer-motion"
+import { motion, Reorder } from "framer-motion";
 
 function ToDoList() {
-
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTasks] = useState("");
+    const [selectedTasks, setSelectedTasks] = useState([]);
+    const [editingTaskIndex, setEditingTaskIndex] = useState(null); // Düzenlenen görevin indeksi
+    const [editingTaskText, setEditingTaskText] = useState(""); // Düzenlenen görev metni
 
     function handleInputChange(event) {
         setNewTasks(event.target.value);
-
     }
 
     function addTask() {
@@ -18,47 +19,99 @@ function ToDoList() {
             setTasks(t => [...t, newTask]);
             setNewTasks("");
         }
-
     }
 
     function deleteTask(index) {
-
-        const updatedTasks = tasks.filter((element, i) => i !== index);
+        const updatedTasks = tasks.filter((_, i) => i !== index);
         setTasks(updatedTasks);
 
+        const updatedSelectedTasks = selectedTasks
+            .filter(i => i !== index)
+            .map(i => (i > index ? i - 1 : i));
 
+        setSelectedTasks(updatedSelectedTasks);
     }
 
     function moveTaskUp(index) {
-
         if (index > 0) {
             const updatedTasks = [...tasks];
             [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
             setTasks(updatedTasks);
-        }
 
+            const updatedSelectedTasks = selectedTasks.map(i => {
+                if (i === index) return index - 1;
+                if (i === index - 1) return index;
+                return i;
+            });
+            setSelectedTasks(updatedSelectedTasks);
+        }
     }
 
     function moveTaskDown(index) {
-
         if (index < tasks.length - 1) {
             const updatedTasks = [...tasks];
             [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
             setTasks(updatedTasks);
+
+            const updatedSelectedTasks = selectedTasks.map(i => {
+                if (i === index) return index + 1;
+                if (i === index + 1) return index;
+                return i;
+            });
+            setSelectedTasks(updatedSelectedTasks);
         }
-
     }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+    };
+
+    function toggleTaskSelection(index) {
+        if (selectedTasks.includes(index)) {
+            setSelectedTasks(st => st.filter(i => i !== index));
+        } else {
+            setSelectedTasks(st => [...st, index]);
+        }
+    }
+
+    function handleReorder(updatedTasks) {
+        setTasks(updatedTasks);
+
+        const updatedSelectedTasks = updatedTasks
+            .map((task, index) => (selectedTasks.includes(tasks.indexOf(task)) ? index : null))
+            .filter(index => index !== null);
+
+        setSelectedTasks(updatedSelectedTasks);
+    }
+
+    function startEditingTask(index) {
+        setEditingTaskIndex(index);  // Düzenleme moduna geç
+        setEditingTaskText(tasks[index]);  // Düzenlenecek görevin mevcut metni
+    }
+
+    function handleTaskUpdate() {
+        if (editingTaskText.trim() !== "") {
+            const updatedTasks = [...tasks];
+            updatedTasks[editingTaskIndex] = editingTaskText;
+            setTasks(updatedTasks);
+            setEditingTaskIndex(null);  // Düzenleme modundan çık
+            setEditingTaskText("");
+        }
+    }
+
     return (
-        <div className="to-do-list" >
-
+        <div className="to-do-list">
             <h1>To-Do-List</h1>
-
             <div>
                 <input
                     type="text"
                     placeholder="Şunu yapacağım.."
                     value={newTask}
-                    onChange={handleInputChange} />
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                />
                 <button
                     className="add-button"
                     onClick={addTask}>
@@ -66,32 +119,76 @@ function ToDoList() {
                 </button>
             </div>
 
-            <Reorder.Group values={tasks} onReorder={setTasks} as="ol">
+            <Reorder.Group values={tasks} onReorder={handleReorder} as="ol">
                 {tasks.map((task, index) => (
-                    <Reorder.Item key={task} value={task} whileDrag={{ scale: 1.05 }}>
-                        <span className="text">{task}</span>
-                        <button
-                            className="delete-button"
-                            onClick={() => deleteTask(index)}>
-                            Sil
-                        </button>
-                        <button
-                            className="move-button"
-                            onClick={() => moveTaskUp(index)}>
-                            ↑
-                        </button>
-                        <button
-                            className="move-button"
-                            onClick={() => moveTaskDown(index)}>
-                            ↓
-                        </button>
+                    <Reorder.Item
+                        key={task}
+                        value={task}
+                        whileDrag={{ scale: 1.05 }}
+                    >
+                        {editingTaskIndex === index ? (  // Eğer düzenleme modundaysak
+                            <div>
+                                <input
+                                    type="text"
+                                    value={editingTaskText}
+                                    onChange={(e) => setEditingTaskText(e.target.value)}
+                                />
+                                <button className='update-task' onClick={handleTaskUpdate}>Kaydet</button>
+                            </div>
+                        ) : (
+                            <>
+                                <label style={{ marginTop: '4px' }}>
+                                    <input
+                                        type='checkbox'
+                                        checked={selectedTasks.includes(index)}
+                                        onChange={() => toggleTaskSelection(index)}
+                                    />
+                                </label>
+                                <motion.button
+                                    className='update-task'
+                                    onClick={() => startEditingTask(index)}
+                                    whileHover={{ scale: 1.2 }}
+                                >
+                                    📝
+                                </motion.button>
+
+                                <span
+                                    className="text"
+                                    style={{
+                                        textDecoration: selectedTasks.includes(index) ? 'line-through' : 'none',
+                                        color: selectedTasks.includes(index) ? 'grey' : 'black',
+                                    }}
+                                >
+                                    {task}
+                                </span>
+
+                                <motion.button
+                                    className="delete-button"
+                                    onClick={() => deleteTask(index)}
+                                    whileHover={{ scale: 1.2 }}
+                                >
+                                    Sil
+                                </motion.button>
+                                <motion.button
+                                    className="move-button"
+                                    onClick={() => moveTaskDown(index)}
+                                    whileHover={{ y: +5 }}>
+                                    ↓
+                                </motion.button>
+                                <motion.button
+                                    className="move-button"
+                                    onClick={() => moveTaskUp(index)}
+                                    whileHover={{ y: -5 }}
+                                >
+                                    ↑
+                                </motion.button>
+                            </>
+                        )}
                     </Reorder.Item>
                 ))}
             </Reorder.Group>
-
-
-        </div>
+        </div >
     );
 }
 
-export default ToDoList
+export default ToDoList;
